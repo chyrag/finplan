d6f31cd55fd6f441a009303322c4fe021d0a7d8c
chirag.kantharia <chirag.kantharia@7dd0af88-8807-11de-8988-25e8a898b690>
2010-01-18 17:58:33 +0000

    Misc changes. This code would be fully rewritten, once I get the hang of what all needs to be done.

--- finance.c
+++ finance.c
@@ -10,38 +10,135 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
 #include <err.h>
 #include <sys/types.h>
+#include "finance.h"
 
 int
-process_file(char *filename)
+get_last_field(char *line)
 {
-	int fd;
-	int buf[BUFSIZ];
-	int lines = 0;
+	char *ptr = strrchr(line, SEPERATOR);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0) {
-		warn("unable to open %s for reading", filename);
-		return -1;
+	if (ptr) {
+		return atoi(ptr+1);
 	}
 
-	memset(buf, 0, BUFSIZ);
-	while (read(fd, buf, BUFSIZ) > 0) {
-		printf("%s\n", buf);
-		lines++;
-	}
-	printf("Processed %d lines\n", lines);
-
-	close(fd);
 	return 0;
 }
 
 int
-display_finance(int verbose)
+process_file(struct monthexp *exp, int months, char *filename)
 {
+	FILE *fp;
+	char buf[BUFSIZ];
+	int i, lines = 0;
+	int amount, savings, savedup = 0;
+
+	fp = fopen(filename, "r");
+	if (!fp) {
+		return -1;
+	}
+
+	memset(buf, 0, BUFSIZ);
+	while (fgets(buf, BUFSIZ, fp)) {
+		switch (buf[0]) {
+			case 'M':
+			case 'B':
+				/* monthly expense */
+				/* handle monthly budgetary expense likewise
+				   for now */
+				amount = get_last_field(buf);
+				if (amount < 0) {
+					/* FIXME: error */
+				}
+				for (i = 0; i < months; i++) {
+					exp[i].expenses += amount;
+				}
+				break;
+			case 'A':
+				/* annual expense */
+				/* read further options */
+				break;
+			case 'U':
+				/* upcoming planned expense */
+				/* read further options */
+				break;
+			case 'S':
+				/* intended savings */
+				savings = get_last_field(buf);
+				if (savedings < 0) {
+					/* FIXME: error */
+				}
+				break;
+			case 'I':
+				/* initial capital */
+				savedup = get_last_field(buf);
+				if (savedup < 0) {
+					/* FIXME: error */
+				}
+				break;
+			case 'G':
+				/* goals */
+				/* read further options */
+				break;
+			default:
+				warn("Unknown option %c", buf[0]);
+				break;
+		}
+		lines++;
+	}
+
+	fclose(fp);
 	return 0;
+}
+
+int
+display_finance(struct monthexp *exp, int months, int verbose)
+{
+	int m;
+
+	for (m = 0; m < months; m++) {
+		printf("%s %d\n", monthnames[exp[m].month], exp[m].year);
+		printf("Expenses: %d\n", exp[m].expenses);
+		printf("Savings: %d\n", exp[m].savings);
+		printf("\n");
+	}
+}
+
+struct monthexp *
+init_expenses(int months)
+{
+	struct monthexp *exp;
+	struct tm *tm;
+	int m, y;
+	time_t now;
+
+	exp = malloc(months * sizeof(struct monthexp));
+	if (exp) {
+		now = time(NULL);
+		tm = localtime(&now);
+
+		for (m = 0, y = 0; m < months; m++) {
+			exp[m].month = (tm->tm_mon + m + 1) % MONTHS_PER_YEAR;
+			if (exp[m].month == 0) {
+				y++;
+			}
+			exp[m].year = tm->tm_year + 1900 + y;
+			exp[m].expenses = 0;
+			exp[m].savings = 0;
+		}
+	}
+
+	return exp;
+}
+
+void
+cleanup_expenses(struct monthexp *expenses)
+{
+	free(expenses);
 }
